407d1de18772c5f1f0e0995c9845d7ddeb75a601
alexr1993 <alexr1993@gmail.com>
2013-09-16 23:03:03 +0100

    Implemented date validation in constructor

--- Date.cpp
+++ Date.cpp
@@ -15,8 +15,9 @@ struct Date {
   Date (int y, int m, int d); // constructor - check validity
   void add_day(int n); // incrememnt date by n days
   enum Month {
-    jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dev
+    jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
   };
+  class Invalid {};
 };
 
 // Date last(2000, 12, 31); colloqially initialises last
@@ -25,6 +26,27 @@ struct Date {
 
 Date::Date(int y, int m, int d) {
 
+
+  if (d <= 0) throw Invalid();
+
+  /* ignoring leap year problem for now */
+  switch (m) {
+
+    case Date::apr: case Date::jun: case Date::sep: case Date::nov:
+      if (d > 30) throw Invalid();
+      break;
+
+    case Date::feb:
+      if (d > 29) throw Invalid();
+      break;
+
+    default: // month must be invalid
+      if (d > 31) throw Invalid();
+  }
+
+  Date::y = y;
+  Date::m = m;
+  Date::d = d;
   // ... need to check constraints and initialise
 }
 
@@ -34,5 +56,7 @@ void Date::add_day(int n) {
 }
 
 int main() {
+
+  Date a(2000, 3, 12);
   return 0;
 }
