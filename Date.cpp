c267c8e97013b5d37e5104097d40cfbdb2517b41
alexr1993 <alexr1993@gmail.com>
2013-09-17 22:46:48 +0100

    Implemented add day/month/year member functions, appears to be working but not fully tested yet

--- Date.cpp
+++ Date.cpp
@@ -11,12 +11,18 @@ using namespace std;
 /* A stuct is essentially an all public class - you cannot effectively impose
    invariants on it's members */
 struct Date {
+  
   int y, m, d;
+  
   Date (int y, int m, int d); // constructor - check validity
   void add_day(int n); // incrememnt date by n days
+  void add_month(int n);
+  void add_year(int n);
+  
   enum Month {
     jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
   };
+  
   class Invalid {};
 };
 
@@ -50,13 +56,60 @@ Date::Date(int y, int m, int d) {
   // ... need to check constraints and initialise
 }
 
+void Date::add_year(int n) {
+  Date::y += n;
+}
+
+void Date::add_month(int n) {
+
+  Date::m = (Date::m + n) % 12;
+  this->add_year(n / 12); // truncation intentional
+}
+
 void Date::add_day(int n) {
 
-  // need to implement
+
+
+  int total = Date::d + n;
+  /* ignoring leap year problem for now */
+  switch (Date::m) {
+
+    case Date::apr: case Date::jun: case Date::sep: case Date::nov:
+      if (total > 30) {
+        this->add_month(1);
+        this->add_day(total - 30);
+      }
+      else Date::d = total;
+      break;
+
+    case Date::feb:
+      if (total > 29) {
+        this->add_month(1);
+        this->add_day(total - 29);
+      }
+      else  Date::d = total;
+
+      break;
+
+    default: // month must be invalid
+      if (total > 31) {
+        this->add_month(1);
+        this->add_day(total - 31);       
+      }
+      else Date::d = total;
+
+  }
+
 }
+
+
 
 int main() {
 
   Date a(2000, 3, 12);
+  a.add_year(2);
+  a.add_month(24);
+  a.add_day(23);
+  cout << a.y << " " << a.m << " " << a.d << endl;
   return 0;
 }
